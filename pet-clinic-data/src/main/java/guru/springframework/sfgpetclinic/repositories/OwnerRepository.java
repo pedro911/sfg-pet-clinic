a2d62636ddf5fc035f7da3c7af86911f170460d2
pedro.marangoni <pedro911@gmail.com>
2019-07-04 14:14:52 +0200

    implement find owners #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,40 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
+
+/*    *//**
+     * Retrieve {@link Owner}s from the data store by last name, returning all owners
+     * whose last name <i>starts</i> with the given name.
+     * @param lastName Value to search for
+     * @return a Collection of matching {@link Owner}s (or an empty Collection if none
+     * found)
+     *//*
+    @Query("SELECT DISTINCT owner FROM Owner owner left join fetch owner.pets WHERE owner.lastName LIKE :lastName%")
+    @Transactional(readOnly = true)
+    Collection<Owner> findByLastName(@Param("lastName") String lastName);
+
+    *//**
+     * Retrieve an {@link Owner} from the data store by id.
+     * @param id the id to search for
+     * @return the {@link Owner} if found
+     *//*
+    @Query("SELECT owner FROM Owner owner left join fetch owner.pets WHERE owner.id =:id")
+    @Transactional(readOnly = true)
+    Owner findById(@Param("id") Integer id);*/
+
+/*      works only with extends Repository
+*     *//**
+     * Save an {@link Owner} to the data store, either inserting or updating it.
+     * @param owner the {@link Owner} to save
+     *//*
+    void save(Owner owner);*/
+
+
 }
